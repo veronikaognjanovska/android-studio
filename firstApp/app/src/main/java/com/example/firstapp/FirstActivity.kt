81df4cd252dc305af8f67a2d7ad4b0cb4037811e
veronikaognjanovska <62549189+veronikaognjanovska@users.noreply.github.com>
2021-11-07 21:38:57 +0100

    Exercise 2

--- firstApp/app/src/main/java/com/example/firstapp/FirstActivity.kt
+++ firstApp/app/src/main/java/com/example/firstapp/FirstActivity.kt
@@ -1,6 +1,8 @@
 package com.example.firstapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,6 +17,12 @@ class FirstActivity : AppCompatActivity() {
         if (bundle != null) {
             val userName = bundle.get("UserName")
             userNameControl.text = "Hello " + userName.toString()
+        }
+
+        var backButton = findViewById<Button>(R.id.backButton)
+        backButton.setOnClickListener {
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
         }
 
     }
